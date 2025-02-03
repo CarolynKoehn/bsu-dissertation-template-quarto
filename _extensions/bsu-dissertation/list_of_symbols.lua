70a1a56326c2198b44429103b0368af535f16ea7
CarolynKoehn <carolynkoehn@u.boisestate.edu>
2025-02-03 16:14:56 -0700

    list of symbols working

--- _extensions/bsu-dissertation/list_of_symbols.lua
+++ _extensions/bsu-dissertation/list_of_symbols.lua
@@ -2,22 +2,22 @@
 
 function Meta(meta)
   -- Define the key in YAML to extract
-  local key = "listAbbreviations"  -- Change this to match the list name in YAML
+  local key = "listSymbols"  -- Change this to match the list name in YAML
   
   if meta[key] and type(meta[key]) == "table" then
     local latex_output = "" -- Store the LaTeX content
     
     for _, item in ipairs(meta[key]) do
-      if type(item) == "table" and item.text and item.definition then
-        latex_output = latex_output .. "\\item[" .. pandoc.utils.stringify(item.text) .. "] " .. pandoc.utils.stringify(item.definition) .. "\n"
+      if type(item) == "table" and item.symbol and item.definition then
+        latex_output = latex_output .. "\\item[$" .. pandoc.utils.stringify(item.symbol) .. "$] " .. pandoc.utils.stringify(item.definition) .. "\n"
       end
     end
     
     -- Wrap in a custom list environment
-    latex_output = "\\begin{listAbbreviations}\n" .. latex_output .. "\\end{listAbbreviations}\n"
+    latex_output = "\\begin{listSymbols}\n" .. latex_output .. "\\end{listSymbols}\n"
     
     -- Write to a file
-    local filepath = "_list_of_abbrevs.tex"
+    local filepath = "_list_of_symbols.tex"
     local f = io.open(filepath, "w")
     if f then
       f:write(latex_output)
